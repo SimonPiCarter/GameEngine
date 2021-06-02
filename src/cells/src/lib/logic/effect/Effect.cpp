8f214698338c2bf4f21fa21c96359d01f96d9eb6
Simon PIERRE <simon.pierre@eurodecision.com>
2021-06-02 18:52:05 +0200

    [cells] logic/effect : added basic effects

--- src/cells/src/lib/logic/effect/Effect.cpp
+++ src/cells/src/lib/logic/effect/Effect.cpp
@@ -1,3 +1,41 @@
 #include "Effect.h"
 
-Effect::Effect() {}
+#include "logic/entity/MobEntity.h"
+
+Effect::Effect(double spawntime_p) : _spawntime(spawntime_p) {}
+
+DamageEffect::DamageEffect(MobEntity * target_p, Tower &tower_p, double damage_p, double spawntime_p)
+	: Effect(spawntime_p)
+	, _target(target_p)
+	, _tower(tower_p)
+	, _damage(damage_p)
+{}
+
+EffectStatus DamageEffect::apply(double elapsedTime_p)
+{
+	EffectStatus status_l;
+	status_l.isOver = true;
+
+	_target->setHitpoint(_target->getHitpoint() - _damage);
+
+	return status_l;
+}
+
+CompositeEffect::CompositeEffect(std::list<Effect *> const &effects_p, double spawntime_p)
+	: Effect(spawntime_p)
+	, _effects(effects_p)
+{}
+
+EffectStatus CompositeEffect::apply(double elapsedTime_p)
+{
+	EffectStatus status_l;
+	status_l.isOver = true;
+
+	// add all composite in effects
+	for(Effect * effect_l : _effects)
+	{
+		status_l.newEffects.push_back({effect_l, 0.});
+	}
+
+	return status_l;
+}
--- src/cells/src/lib/logic/effect/Effect.h
+++ src/cells/src/lib/logic/effect/Effect.h
@@ -15,8 +15,39 @@ struct EffectStatus
 class Effect
 {
 public:
-	Effect();
+	Effect(double spawntime_p);
 
 	/// @brief apply the effect and return the status
+	virtual EffectStatus apply(double elapsedTime_p) = 0;
+
+protected:
+	double const _spawntime;
+};
+
+class MobEntity;
+class Tower;
+
+class DamageEffect : public Effect
+{
+public:
+	DamageEffect(MobEntity * target_p, Tower &tower_p, double damage_p, double spawntime_p);
+
 	EffectStatus apply(double elapsedTime_p);
+
+protected:
+	MobEntity * const _target;
+	Tower & _tower;
+	double const _damage;
+};
+
+
+class CompositeEffect : public Effect
+{
+public:
+	CompositeEffect(std::list<Effect *> const &effects_p, double spawntime_p);
+
+	EffectStatus apply(double elapsedTime_p);
+
+protected:
+	std::list<Effect *> _effects;
 };
