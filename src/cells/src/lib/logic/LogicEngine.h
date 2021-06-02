2ecbc7f9f58684212f88393fc9e7d75bad12ee9d
Simon PIERRE <simon.pierre@eurodecision.com>
2021-06-02 18:50:29 +0200

    [cells] logic : added towers

--- src/cells/src/lib/logic/LogicEngine.h
+++ src/cells/src/lib/logic/LogicEngine.h
@@ -3,6 +3,7 @@
 #include "layout/MapLayout.h"
 
 class WaveEngine;
+class Tower;
 
 class LogicEngine
 {
@@ -13,11 +14,18 @@ public:
 
 	void setCurrentMap(MapLayout const * map_p) { _currentMap = map_p; }
 
+	std::list<Tower *> const & getTowers() const { return _towers; }
+	std::list<Tower *> & getTowers() { return _towers; }
+
 	void run();
 protected:
 	bool _quit;
 
+	/// @brief map
 	MapLayout const * _currentMap;
+
+	/// @brief list of tower
+	std::list<Tower *> _towers;
 
 	/// @brief tandem class can access logic data
 	friend class WaveEngine;
