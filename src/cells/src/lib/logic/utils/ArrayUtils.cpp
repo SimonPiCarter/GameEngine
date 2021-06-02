7b2701de30b112d3140c384161c0bfab840c0a6b
Simon PIERRE <simon.pierre@eurodecision.com>
2021-06-02 18:51:32 +0200

    [cells] logic/utils : added square_distance

--- src/cells/src/lib/logic/utils/ArrayUtils.cpp
+++ src/cells/src/lib/logic/utils/ArrayUtils.cpp
@@ -7,3 +7,8 @@ std::array<double, 2> normalize(std::array<double, 2> const &array_p)
 	double length_l = sqrt(array_p[0]*array_p[0] + array_p[1]*array_p[1]);
 	return { array_p[0]/length_l, array_p[1]/length_l };
 }
+
+double square_distance(std::array<double, 2> const &pos1_p, std::array<double, 2> const &pos2_p)
+{
+	return (pos1_p[0]-pos2_p[0])*(pos1_p[0]-pos2_p[0]) + (pos1_p[1]-pos2_p[1])*(pos1_p[1]-pos2_p[1]);
+}
--- src/cells/src/lib/logic/utils/ArrayUtils.h
+++ src/cells/src/lib/logic/utils/ArrayUtils.h
@@ -3,3 +3,5 @@
 #include <array>
 
 std::array<double, 2> normalize(std::array<double, 2> const &array_p);
+
+double square_distance(std::array<double, 2> const &pos1_p, std::array<double, 2> const &pos2_p);
