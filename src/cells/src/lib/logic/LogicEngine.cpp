2e70ee806440b5868acce98cdac3979b9768682c
Simon PIERRE <simon.pierre@eurodecision.com>
2021-06-03 16:07:22 +0200

    [cells] logic : added LogicEngine one wave handling (wip)

--- src/cells/src/lib/logic/LogicEngine.cpp
+++ src/cells/src/lib/logic/LogicEngine.cpp
@@ -4,6 +4,7 @@
 #include "logic/WaveEngine.h"
 #include "logic/display/MapDisplay.h"
 #include "logic/entity/Tower.h"
+#include "logic/layout/WaveLayout.h"
 
 LogicEngine::LogicEngine(MapLayout const * map_p, CellsEngine * cellsEngine_p)
 	: _cellsEngine(cellsEngine_p)
@@ -23,8 +24,6 @@ LogicEngine::~LogicEngine()
 
 void LogicEngine::init()
 {
-	delete _waveEngine;
-	_waveEngine = new WaveEngine(*this);
 	if(_cellsEngine)
 	{
 		MapDisplay * display_l = newMapDisplay("root", _currentMap, _cellsEngine->getGraphic());
@@ -36,15 +35,40 @@ void LogicEngine::init()
 		_cellsEngine->getGraphic().registerMessage(new MoveCameraMessage({size_l*1.2, size_l/2. - 0.5, size_l}));
 		_cellsEngine->getGraphic().registerMessage(new LookAtCameraMessage({size_l/2., size_l/2. - 0.5, 0.}));
 		_cellsEngine->getGraphic().registerMessage(new RotateCameraMessage({0., 0., 90.}));
-
-		//_waveEngine->init(??);
 	}
+}
+
+WaveLayout getNextWave()
+{
+	WaveLayout layout_l;
+	layout_l.mobLayout.push_back(
+		{
+			{12., 1., ArmorType::Standard, "Cube", {0.1,0.1}},		// Mob model
+			10,														// number of spawn
+			1.														// interval
+		}
+	);
+	return layout_l;
 }
 
 void LogicEngine::run(double elapsedTime_p)
 {
+	bool startWave_l = true;
+	WaveLayout wave_l = getNextWave();
+
+	if(!_waveEngine && startWave_l)
+	{
+		_waveEngine = new WaveEngine(*this);
+		_waveEngine->init(wave_l);
+		startWave_l = false;
+	}
 	if(_waveEngine)
 	{
 		_waveEngine->handleFrame(elapsedTime_p);
+		if(_waveEngine->isWaveOver())
+		{
+			delete _waveEngine;
+			_waveEngine = nullptr;
+		}
 	}
 }
