247e32bfbce1cb66c3995fa293207c62f31f91ac
Simon PIERRE <simon.pierre@eurodecision.com>
2021-06-08 23:51:36 +0200

    [cells] logic/types : added stream operator

--- src/cells/src/lib/logic/types/ArmorType.h
+++ src/cells/src/lib/logic/types/ArmorType.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 enum class ArmorType
 {
 	Standard,
@@ -7,3 +9,23 @@ enum class ArmorType
 	Heavy,
 	Light
 };
+
+inline std::ostream & operator<<(std::ostream & os_p, ArmorType type_p)
+{
+	switch(type_p)
+	{
+		case ArmorType::Standard:
+			os_p<<"Standard";
+			break;
+		case ArmorType::Resilient:
+			os_p<<"Resilient";
+			break;
+		case ArmorType::Heavy:
+			os_p<<"Heavy";
+			break;
+		case ArmorType::Light:
+			os_p<<"Light";
+			break;
+	}
+	return os_p;
+}
