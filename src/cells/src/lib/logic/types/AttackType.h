706d983c200f9f40d61312e5e51f5491086f6c89
Simon PIERRE <simon.pierre@eurodecision.com>
2021-06-11 22:43:12 +0200

    [cells] logic/slot : added getDesc for AttackModifier

--- src/cells/src/lib/logic/types/AttackType.h
+++ src/cells/src/lib/logic/types/AttackType.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include <stdexcept>
+
 enum class AttackType
 {
 	Direct,
@@ -7,3 +10,19 @@ enum class AttackType
 	Splash,
 	Line
 };
+
+inline std::string to_string(AttackType type_p)
+{
+	switch(type_p)
+	{
+		case AttackType::Direct:
+			return "Direct";
+		case AttackType::Arc:
+			return "Arc";
+		case AttackType::Splash:
+			return "Splash";
+		case AttackType::Line:
+			return "Line";
+	};
+	throw std::logic_error("missing implem for AttackType");
+}
